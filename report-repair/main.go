8762b56fc5a638cfea92c04a57c3f91b8822b81c
Juanes Echavarria <juanesech@gmail.com>
2020-12-01 19:46:42 -0500

    [FIX] variable names

--- report-repair/main.go
+++ report-repair/main.go
@@ -10,11 +10,11 @@ func main()  {
   var sum = 2020
   input_content, err := L.ReadLines("input.txt")
   input := L.ArrayToInt(input_content)
-  i2_numbers, i2_product := L.Iterator(input, 2, sum)
-  i3_numbers, i3_product := L.Iterator(input, 3, sum)
+  i2Numbers, i2_product := L.Iterator(input, 2, sum)
+  i3Numbers, i3_product := L.Iterator(input, 3, sum)
   if err != nil {
       log.Fatalf("readLines: %s", err)
   }
-  fmt.Println("2 Numbers", i2_numbers, "Product: ", i2_product)
-  fmt.Println("3 Numbers", i3_numbers, "Product: ", i3_product)
+  fmt.Println("2 Numbers", i2Numbers, "Product: ", i2_product)
+  fmt.Println("3 Numbers", i3Numbers, "Product: ", i3_product)
 }
