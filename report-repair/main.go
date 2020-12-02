c906a7701dbac8146e178066cc97aff0af36d7f4
Juan Esteban Echavarria O <jechavarria@contractor.ea.com>
2020-12-02 10:30:49 -0500

    [REFACTOR] utils

--- report-repair/main.go
+++ report-repair/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  L "./lib"
+	"fmt"
+	"log"
+	U "../utils"
+	L "./lib"
 )
 
-func main()  {
-  var sum = 2020
-  fileContent, err := L.ReadLines("input.txt")
-  input := L.ArrayToInt(fileContent)
-  i2Numbers, i2Product := L.Iterator(input, 2, sum)
-  i3Numbers, i3Product := L.Iterator(input, 3, sum)
-  if err != nil {
-      log.Fatalf("readLines: %s", err)
-  }
-  fmt.Println("2 Numbers", i2Numbers, "Product: ", i2Product)
-  fmt.Println("3 Numbers", i3Numbers, "Product: ", i3Product)
+func main() {
+	var sum = 2020
+	fileContent, err := U.ReadLines("input.txt")
+	input := L.ArrayToInt(fileContent)
+	i2Numbers, i2Product := L.Iterator(input, 2, sum)
+	i3Numbers, i3Product := L.Iterator(input, 3, sum)
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+	fmt.Println("2 Numbers", i2Numbers, "Product: ", i2Product)
+	fmt.Println("3 Numbers", i3Numbers, "Product: ", i3Product)
 }
